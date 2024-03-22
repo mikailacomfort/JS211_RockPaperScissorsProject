12c77ae452cd00b087742fbc1d66fd4e783635c6
mikailacomfort <mikailacomfort@gmail.com>
2024-03-21 20:58:02 -0500

    added functionality

--- main.js
+++ main.js
@@ -13,11 +13,28 @@ const rl = readline.createInterface({
 
 // the function that will be called by the unit test below
 const rockPaperScissors = (hand1, hand2) => {
+  hand1 = hand1.trim().toLowerCase();
+  hand2 = hand2.trim().toLowerCase();
+  
+  if (!['rock', 'paper', 'scissors'].includes(hand1) || !['rock', 'paper', 'scissors'].includes(hand2)) {
+      return "Invalid input. Please enter 'rock', 'paper', or 'scissors'.";
+  }
 
-  // Write code here
-  // Use the unit test to see what is expected
-
+  if (hand1 === hand2) {
+    return "It's a tie!";
+  } else if (
+      (hand1 === 'rock' && hand2 === 'scissors') ||
+      (hand1 === 'paper' && hand2 === 'rock') ||
+      (hand1 === 'scissors' && hand2 === 'paper')
+  ) {
+      return "Hand one wins!";
+  } else {
+      return "Hand two wins!";
+  }
 }
+  // Use the unit test to see what is expected
+ 
+
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
